28cf8d5b747c1811263c6621da4226479316affc
Kenyi Hancco <kenyi.hancco@cloudcomputing.com.pe>
2021-11-17 10:18:56 -0500

    Se utilza el paradigma POO y se empieza a construir el juego

---
+++ game-dist.js
@@ -0,0 +1,31 @@
+;(function() {
+
+    class Square {
+        constructor(x, y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        draw() {
+            ctx.fillRect(this.x, this.y, 10, 10);
+        }
+
+    }
+
+    class Snake {
+        constructor() {
+            this.head = new Square(100, 0);
+            this.draw();
+        }
+
+        draw() {
+            this.head.draw();
+        }
+    }
+
+
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    
+    const snake = new Snake();
+})();
